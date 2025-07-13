e7bb04184dc9cc71b74f04972e85ad435a01ae3d
Danner Kurtz <kurtz.danner@gmail.com>
2025-07-13 02:02:42 -0300

    backend finalizado

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
-import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
-import { z } from 'zod/v4';
-import { db } from '../../db/connections.ts';
-import { schemas } from '../../db/schema/index.ts';
+import { and, eq, sql } from 'drizzle-orm'
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+import { db } from '../../db/connections.ts'
+import { schemas } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../service/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -12,31 +14,59 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
           roomId: z.string(),
         }),
         body: z.object({
-          question: z.string(),
+          question: z.string().min(1),
         }),
       },
     },
     async (request, reply) => {
-      const { roomId } = request.params;
-      const { question } = request.body;
+      const { roomId } = request.params
+      const { question } = request.body
+
+      const embeddings = await generateEmbeddings(question)
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+
+      const chunks = await db
+        .select({
+          id: schemas.audioChunks.id,
+          transcription: schemas.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schemas.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schemas.audioChunks)
+        .where(
+          and(
+            eq(schemas.audioChunks.roomId, roomId),
+            sql`1 - (${schemas.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schemas.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
 
       const result = await db
         .insert(schemas.questions)
-        .values({
-          roomId,
-          question,
-        })
-        .returning();
+        .values({ roomId, question, answer })
+        .returning()
 
-      const insertedRoom = result[0];
+      const insertedQuestion = result[0]
 
-      if (!insertedRoom) {
-        throw new Error('Error creating question');
+      if (!insertedQuestion) {
+        throw new Error('Failed to create new room.')
       }
 
-      return reply.code(201).send({
-        questionId: insertedRoom.id,
-      });
+      return reply.status(201).send({
+        questionId: insertedQuestion.id,
+        answer,
+      })
     }
-  );
-};
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ import { createQuestionRoute } from './http/routes/create-question.ts';
 import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomQuestions } from './http/routes/get-room-questions.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { uploadAudioRoute } from './http/routes/upload-audio.ts';
+import fastifyMultipart from '@fastify/multipart';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,7 +20,7 @@ export default app;
 app.register(fastifyCors, {
   origin: true,
 });
-
+app.register(fastifyMultipart)
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
@@ -30,5 +32,7 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
+
 
 app.listen({ port: env.PORT });
